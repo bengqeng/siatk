d111017eb7f426a23b02683c366d5b84ed6c966c
Adrian <adrian.rahmandanu2@gmail.com>
2020-09-13 11:52:27 +0700

    set hook for development for auto migrate and create database

--- application/config/development/hooks.php
+++ application/config/development/hooks.php
@@ -11,3 +11,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+$hook['pre_system'] = array(  
+                    'class' => 'Initdb',  
+                    'function' => 'test_db',  
+                    'filename' => 'Initdb.php',  
+                    'filepath' => 'hooks/local'
+                    );
+
+$hook['pre_controller'] = array(  
+        'class' => 'Automigrate',  
+        'function' => 'migrate_db_on_first_load',  
+        'filename' => 'Automigrate.php',  
+        'filepath' => 'hooks/local',  
+        );    
+  
